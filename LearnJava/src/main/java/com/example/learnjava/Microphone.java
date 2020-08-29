93754196f9413081fc612dba9a4d5272cf27b2cc
Deyvison Silas <deyvisonsilas122@gmail.com>
2020-08-28 21:17:06 -0300

    Constructor overload

--- LearnJava/src/main/java/com/example/learnjava/Microphone.java
+++ LearnJava/src/main/java/com/example/learnjava/Microphone.java
@@ -3,9 +3,11 @@ package com.example.learnjava;
 public class Microphone
 {
     //  Instance variables |or| Properties |or| Fields
-    String name;
-    String color;
-    int model;
+    private String name;
+    private String color;
+    private int model;
+
+    public Microphone(){}
 
     public Microphone(String name, String color, int model)
     {
@@ -14,18 +16,57 @@ public class Microphone
         this.model = model;
     }
 
+    public Microphone(String name, int model)
+    {
+        this.name = name;
+        this.model = model;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getColor()
+    {
+        return color;
+    }
+
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+
+    public int getModel()
+    {
+        return model;
+    }
+
+    public void setModel(int model)
+    {
+        this.model = model;
+    }
+
     public void turnOn()
     {
         System.out.println(this.name + " is turned On");
     }
+
     public void turnOff()
     {
         System.out.println(this.name + "is turned Off");
     }
+
     public void setVolume()
     {
         System.out.println("Setting volume...");
     }
+
     public String showDescription()
     {
         return "Microphone name " + name + " with the color " + color + " and the model number " + model;
--- LearnJava/src/main/java/com/example/learnjava/MyClass.java
+++ LearnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -5,17 +5,40 @@ public class MyClass
     public static void main(String[] args)
     {
         Microphone microphone = new Microphone("Xingling00","Red", 12345);
-//        microphone.color = "Red";
-//        microphone.name = "Samsung A10";
-//        microphone.model = 2345;
-
+        microphone.setColor("Red");
+        microphone.setName("MyPhoneChingLing");
+        microphone.setModel(77777);
+        //
         microphone.turnOn();
         microphone.setVolume();
         microphone.turnOff();
-
         System.out.println(microphone.showDescription());
 
-        System.out.println(Sum(1,2,3,4));
+        //  Line separator
+        System.out.println(System.getProperty("line.separator"));
+        //
+
+        Microphone newMic = new Microphone();
+        newMic.setColor("Blue");
+        newMic.setName("BlueMic");
+        newMic.setModel(192837);
+        //
+        newMic.turnOn();
+        newMic.setVolume();
+        newMic.turnOff();
+        System.out.println(newMic.showDescription());
+
+        //  Line separator
+        System.out.println(System.getProperty("line.separator"));
+        //
+
+        Microphone otherMic = new Microphone("myOtherMic", 8219);
+        otherMic.turnOn();
+        otherMic.setVolume();
+        otherMic.turnOff();
+        System.out.println(otherMic.showDescription());
+
+//        System.out.println(Sum(1,2,3,4));
     }
     public static int Sum(int... numbers)
     {
